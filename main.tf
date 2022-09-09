edd873495c01075e53a300b8b76b3e7beee65763
Parisa Moosavinezhad <parisam@microsoft.com>
2022-09-09 15:24:42 +0200

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_user_assigned_identity" "this" {
+  name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
-  name                = var.name
   tags = merge(
     var.additional_tags,
     {
